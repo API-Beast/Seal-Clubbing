0a56b0e7c0efc9f6598af8d4c3cac48a32df056e
Thygrrr <github@thygrrr.de>
2018-02-24 17:22:26 +0100

    new floot

--- glowtile.gd
+++ glowtile.gd
@@ -3,11 +3,11 @@ extends Sprite
 # class member variables go here, for example:
 
 
-var anis = ["turn_off", "turn_on", "quick_flash", "short_pulse", "short_pulse", "none", "none", "none", "none"]
-var colors = ["C95AEE", "5F9AEE", "F5CB6E"]
+var anis = ["turn_off", "turn_on", "smooth", "smooth", "smooth", "quick_flash", "short_pulse", "short_pulse", "none", "none", "none", "none", "none", "none", "none", "none"]
+var colors = [Color("C95AEE"), Color("5F9AEE"), Color("F5CB6E")]
 export var speed = 1.0
 export var pulse = 0.0
-var bg = Color("3E3146")
+var bg = Color("211A26")
 var color = bg
 
 
@@ -21,7 +21,7 @@ func _process(delta):
 	if not $AnimationPlayer.is_playing():
 		for i in range(0,10):
 			$AnimationPlayer.queue(anis[randi()%anis.size()])
-			color = Color(colors[randi()%colors.size()])
+			color = colors[randi()%colors.size()]
 	
 	#Blend animation color to background color
 	$".".modulate = bg.linear_interpolate(color, pulse)
