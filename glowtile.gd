03467da7a8101e807c483e36b2954ec1b9330fc4
Thygrrr <github@thygrrr.de>
2018-02-24 15:41:37 +0100

    floor colors and layout

--- glowtile.gd
+++ glowtile.gd
@@ -4,17 +4,18 @@ extends Sprite
 
 
 var anis = ["turn_off", "turn_on", "quick_flash", "short_pulse", "short_pulse", "none", "none", "none", "none"]
+var colors = ["C95AEE", "5F9AEE", "F5CB6E", "3E3146"]
+var speed = 2.0
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	$AnimationPlayer.play("turn_off");
-	$AnimationPlayer.playback_speed = 2.0
+	$AnimationPlayer.playback_speed = speed
 	pass
 
 func _process(delta):
 	if not $AnimationPlayer.is_playing():
 		for i in range(0,10):
 			$AnimationPlayer.queue(anis[randi()%anis.size()])
-			
+			$".".self_modulate = Color(colors[randi()%colors.size()])
 			
